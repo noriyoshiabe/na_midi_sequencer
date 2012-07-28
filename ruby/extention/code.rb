e638ad983d1c7b893c55e911160c28629fbc5534
masaori <masaori@pankaku.com>
2012-07-29 01:39:33 +0900

    [code_input] 各種機能追加
    ・テンションノートの入力
    ・ボイシングパターンの指定

--- ruby/extention/code.rb
+++ ruby/extention/code.rb
@@ -8,18 +8,37 @@ class Context
 	@@convert_table.merge! ROOT_NAME_TABLE
 
 	SUFFIX_DEFINITIONS = {
-		'M'    => [24, 60, 64, 67],
-		'm'    => [24, 60, 63, 67],
-		'dim'  => [24, 60, 63, 66],
-		'aug'  => [24, 60, 64, 68],
-		'M7'   => [24, 60, 64, 67, 71],
-		'7'    => [24, 60, 64, 67, 70],
-		'6'    => [24, 60, 64, 67, 69],
-		'mM7'  => [24, 60, 63, 67, 71],
-		'm7'   => [24, 60, 63, 67, 70],
-		'm6'   => [24, 60, 63, 67, 69],
-		'dim7' => [24, 60, 63, 66, 69],
-		'aug7' => [24, 60, 64, 68, 70],
+		'M'    => [0, 4, 7],
+		'm'    => [0, 3, 7],
+		'dim'  => [0, 3, 6],
+		'aug'  => [0, 4, 8],
+		'sus4' => [0, 5, 7],
+		'M7'   => [0, 4, 7, 11],
+		'7'    => [0, 4, 7, 10],
+		'6'    => [0, 4, 7, 9],
+		'mM7'  => [0, 3, 7, 11],
+		'm7'   => [0, 3, 7, 10],
+		'm6'   => [0, 3, 7, 9],
+		'dim7' => [0, 3, 6, 9],
+		'aug7' => [0, 4, 8, 10],
+		'sus7' => [0, 5, 7, 10],
+	}
+
+	TENSION_NOTE_ALIAS = {
+		'b9'  => 1,
+		'9'   => 2,
+		'#9'  => 3,
+		'11'  => 5,
+		'#11' => 6,
+		'b13' => 8,
+		'13'  => 9,
+	}
+
+	BUILT_IN_VOICING_PATTERN_ALIAS = {
+		'close'           => '||0123456',
+		'open'            => '||0213465',
+		'close_with_bass' => 'R||0123456',
+		'open_with_bass'  => 'R||0213465',
 	}
 
 	def code(h)
@@ -28,14 +47,80 @@ class Context
 		root_num = convert h[:rt]
 		raise ":rt can't be parsed" if root_num.nil?
 
+		tension_notes = parse_tention_note(h[:tn])
+
 		suffix = (h[:sf].nil? || h[:sf].empty?) ? 'M' : h[:sf]
 		raise ":sf can't be parsed" if !SUFFIX_DEFINITIONS.has_key?(suffix)
-
 		definition = SUFFIX_DEFINITIONS[suffix]
-		for i in 0..definition.length-1
-			interval = definition[i]
-			note :n => root_num + interval, :c => h[:c], :s => (i==definition.length-1) ? h[:s] : 0, :g => h[:g], :v => h[:v]
+		definition += tension_notes
+
+		voicing_pattern = ''
+		if !h[:vp].nil? && !h[:vp].empty?
+			built_in = BUILT_IN_VOICING_PATTERN_ALIAS[h[:vp]]
+			voicing_pattern = built_in.nil? ? h[:vp] : built_in
+		else
+			voicing_pattern = 'R||0123456'
+		end
+
+		notes = create_voicing(root_num, definition, voicing_pattern)
+		p notes
+
+		for i in 0..notes.length-1
+			n = notes[i]
+			note :n => n, :c => h[:c], :s => (i==notes.length-1) ? h[:s] : 0, :g => h[:g], :v => h[:v]
 		end
 	end
 
+	def parse_tention_note(input)
+		result = []
+
+		if input.kind_of?(String)
+			tention_num = TENSION_NOTE_ALIAS[input]
+			raise ":tn can't be parsed" if tention_num.nil?
+			result << tention_num
+		elsif input.kind_of?(Array)
+			input.each {|s|
+				tention_num = TENSION_NOTE_ALIAS[s]
+				raise ":tn can't be parsed" if tention_num.nil?
+				result << tention_num
+			}
+		elsif input.nil?
+			return []
+		else
+			raise ":tn must be String or Array"
+		end
+
+		result
+	end
+
+	def create_voicing(root_num, suffix_definition, voicing_pattern)
+		result = []
+		current_octave = 24
+		current_note = 0
+
+		voicing_pattern.each_char {|c|
+			c = '0' if 'R' == c 
+			
+			case c
+			when /[0-9]/
+				current_note = suffix_definition[c.to_i]
+				next if current_note.nil?
+				current_note += root_num
+			when '|'
+				current_octave += 12
+				next
+			else
+				raise ':vp voicing pattern is invalid string'
+			end
+			if !result.last.nil?
+				until result.last <= current_note + current_octave
+					current_octave += 12
+				end
+			end
+			
+			result << current_note + current_octave
+		}
+
+		result
+	end
 end
--- ruby/test/code_test.rb
+++ ruby/test/code_test.rb
@@ -12,10 +12,10 @@ end
 
 
 def piano_code_play
-	code :rt => 'E',  :sf => 'M7',   :s => 48, :g => 48, :v => 90
-	code :rt => 'C#', :sf => 'dim7', :s => 48, :g => 48, :v => 90
-	code :rt => 'D',  :sf => 'm7',   :s => 48, :g => 48, :v => 90
-	code :rt => 'G',  :sf => '7',    :s => 48, :g => 48, :v => 90
-	code :rt => 'C',  :sf => '6',    :s => 48, :g => 48, :v => 90
+	code :rt => 'E',                 :tn => '9',           :vp => 'R||0412356',      :s => 48, :g => 48, :v => 90 
+	code :rt => 'D#', :sf => 'dim7',                       :vp => 'open_with_bass',  :s => 48, :g => 48, :v => 90
+	code :rt => 'D',  :sf => 'm7',                                                   :s => 48, :g => 48, :v => 90
+	code :rt => 'G',  :sf => '7',    :tn => ['b9', 'b13'], :vp => 'close_with_bass', :s => 48, :g => 48, :v => 90
+	code :rt => 'C',  :sf => 'M7',   :tn => ['9', '#11'],  :vp => 'R20|3153',       :s => 48, :g => 48, :v => 90
 
 end
