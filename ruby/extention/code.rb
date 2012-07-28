62009117429f3f8b7dd4e30e0bba2605444af92b
masaori <masaori@pankaku.com>
2012-07-28 23:20:09 +0900

    [code_input] サフィックス指定してコード鳴らせるようにした

--- ruby/extention/code.rb
+++ ruby/extention/code.rb
@@ -7,15 +7,35 @@ class Context
 
 	@@convert_table.merge! ROOT_NAME_TABLE
 
+	SUFFIX_DEFINITIONS = {
+		'M'    => [24, 60, 64, 67],
+		'm'    => [24, 60, 63, 67],
+		'dim'  => [24, 60, 63, 66],
+		'aug'  => [24, 60, 64, 68],
+		'M7'   => [24, 60, 64, 67, 71],
+		'7'    => [24, 60, 64, 67, 70],
+		'6'    => [24, 60, 64, 67, 69],
+		'mM7'  => [24, 60, 63, 67, 71],
+		'm7'   => [24, 60, 63, 67, 70],
+		'm6'   => [24, 60, 63, 67, 69],
+		'dim7' => [24, 60, 63, 66, 69],
+		'aug7' => [24, 60, 64, 68, 70],
+	}
+
 	def code(h)
 		raise ":rt must not be nil" if h[:rt].nil?
 
-		root_num = convert(h[:rt])
+		root_num = convert h[:rt]
 		raise ":rt can't be parsed" if root_num.nil?
 
-		note :n => root_num + 72    , :c => h[:c], :s => h[:s], :g => h[:g], :v => h[:v]
-		note :n => root_num + 72 + 4, :c => h[:c], :s => h[:s], :g => h[:g], :v => h[:v]
-		note :n => root_num + 72 + 7, :c => h[:c], :s => h[:s], :g => h[:g], :v => h[:v]
+		suffix = (h[:sf].nil? || h[:sf].empty?) ? 'M' : h[:sf]
+		raise ":sf can't be parsed" if !SUFFIX_DEFINITIONS.has_key?(suffix)
+
+		definition = SUFFIX_DEFINITIONS[suffix]
+		for i in 0..definition.length-1
+			interval = definition[i]
+			note :n => root_num + interval, :c => h[:c], :s => (i==definition.length-1) ? h[:s] : 0, :g => h[:g], :v => h[:v]
+		end
 	end
 
 end
--- ruby/test/code_test.rb
+++ ruby/test/code_test.rb
@@ -2,7 +2,7 @@ require 'code'
 
 def preprocessing
 	patch_change :c => 9, :m => 0x7F, :l => 0x00, :p => 0x07
-	tempo_change :t => 100.0
+	tempo_change :t => 72.0
 end
 
 def play
@@ -12,6 +12,10 @@ end
 
 
 def piano_code_play
-	code :rt => 'C', :g => 192, :v => 90
+	code :rt => 'E',  :sf => 'M7',   :s => 48, :g => 48, :v => 90
+	code :rt => 'C#', :sf => 'dim7', :s => 48, :g => 48, :v => 90
+	code :rt => 'D',  :sf => 'm7',   :s => 48, :g => 48, :v => 90
+	code :rt => 'G',  :sf => '7',    :s => 48, :g => 48, :v => 90
+	code :rt => 'C',  :sf => '6',    :s => 48, :g => 48, :v => 90
 
 end
